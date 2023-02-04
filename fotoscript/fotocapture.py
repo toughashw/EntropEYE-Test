2002dcdfc2fd78b4bd03be1a910bedd36b9d68ee
Alessandro Carucci <alessandrocarucci.ac@gmail.com>
2023-02-04 18:54:22 +0000

    reorder code

--- fotoscript/fotocapture.py
+++ fotoscript/fotocapture.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 import time
+import datetime
 
 camera = PiCamera()
 time.sleep(2)
@@ -7,8 +8,10 @@ camera.resolution = (1920, 1080)
 camera.vflip = True
 camera.contrast = 10
 
- timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    picture_filename = "/home/pi/Desktop/EntropEYE-Camera/fotocapture/picture_{}.jpg".format(timestamp)
+timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+picture_filename = "/home/pi/Desktop/EntropEYE-Camera/fotocapture/image_{}.jpg".format(timestamp)
 
+print("Sto scattando...")
 camera.capture(picture_filename)
-print("Done.")
+
+print("Fatto")
--- videoscript/videocapture.py
+++ videoscript/videocapture.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 import time
+import datetime
 
 camera = PiCamera()
 time.sleep(2)
@@ -7,13 +8,13 @@ camera.resolution = (1280, 720)
 camera.vflip = True
 camera.contrast = 10
 
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-video_filename = "/home/pi/Desktop/EntropEYE-Camera/fotocapture/video_{}.h264".format(timestamp)
+timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+video_filename = "/home/pi/Desktop/EntropEYE-Camera/videocapture/video_{}.h264".format(timestamp)
 
-print("Start recording...")
+print("Sto registrando...")
 camera.start_recording(video_filename)
 camera.wait_recording(30)
 camera.stop_recording()
 
 !MP4Box -add video_filename.h264 video_filename.mp4
-print("Done.")
+print("Fatto")
